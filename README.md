13bfd4ccfaceb07a65c20cfea42218a27ac758e7
Zach Newbery <47321182+ZachNewbery@users.noreply.github.com>
2025-02-12 12:55:29 +0200

    2025 Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# zachnewbery.github.io
+This website details my personal achievements, education and projects undertaken.
+
+It acts as a web-based CV, and provides a way for me to learn more about web development.
---
+++ helpers.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navbar = document.querySelector('#navbar');
+    var navTop = navbar.offsetTop;
+    document.getElementById("defaultLang").click();
+    window.addEventListener('scroll', checkNav);
+
+    function checkNav() {
+        if (window.pageYOffset >= navTop) {
+          document.body.style.paddingTop = 0;
+          navbar.classList.add("sticky");
+        } else {
+          document.body.style.paddingTop = nav.offsetHeight + 'px';
+          navbar.classList.remove("sticky");
+        }
+    }
+})
+
+function openProjects(evt, language) {
+    var i, content, link;   
+
+    content = document.getElementsByClassName("project-content");
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+
+    link = document.getElementsByClassName("language");
+    for (i = 0; i < link.length; i++) {
+        link[i].className = link[i].className.replace(" active", "");
+    }
+
+    document.getElementById(language).style.display = "block";
+    evt.currentTarget.className += " active";    
+}
+
+$(document).ready(function(){
+    $("#home-link").click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    $("#about-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#profile").offset().top - 50
+        }, 1000);
+    });
+    $("#resume-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#resume").offset().top - 50 // Magic Number because weird scrolling?
+        }, 1000);
+    });
+    $("#education-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#education").offset().top - 50
+        }, 1000);
+    });
+    $("#experience-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#experience").offset().top - 50
+        }, 1000);
+    });
+    $("#awards-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#awards").offset().top - 50
+        }, 1000);
+    });
+    $("#volunteering-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#volunteering").offset().top - 50
+        }, 1000);
+    });
+    $("#skills-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top - 250
+        }, 1000);
+    });
+    $("#project-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#project-section").offset().top - 50
+        }, 1000);
+    });
+    $("#contact-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $(document).height()
+        }, 1000);
+    });
+    $(".icon").click(function() {
+        $(".icon").toggleClass("active");
+        $("#menu").slideToggle();
+    });
+    $("#profile").hide();
+    $("#profile").fadeIn(1500);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
